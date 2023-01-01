c69bee685bf1d8293d736f4ca8d213ede70ffa30
shreshthajit5@gmail.com <shreshthajit5@gmail.com>
2023-01-02 04:49:33 +0600

    1st commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/setAvatar" element={<SetAvatar />} /> */}
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
